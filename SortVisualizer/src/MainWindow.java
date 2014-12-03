088f579b5bc2808726ded4442bfa44c6cbc64371
MattDs17 <matt.dsouza@gmail.com>
2014-12-03 15:31:47 -0500

    General GUI and Text Input
    Configured a significant portion of the Swing components. Four different
    sortPanels (subclasses of JPanel) added to the Frame for selection,
    insertion, merge, and quick sort. Buttons and instructions added to take
    values from adjacent JTextPane.
    
    Method added which parses a String (in general from JTextPane) and
    parses all positive integers using regex to prevent exceptions from
    being thrown. The valid numbers in the String are added to each of the
    four sortPanels and then everything is repainted.
    
    Ideas/To do:
    -code the algorithms themselves
    -configure a delay (and perhaps a pause button) to show steps
    -add JLabels to each sortPanel describing the instruction/step
    -add a colour array to sortPanel to indicate what color to make each bar
    based on state (sorted, unsorted, greater than, less than, not relevant
    at the moment, etc.)
    -have the system run the sorts once without delay to compare time
    elapsed
    -consider removing start button and having each sortPanel start/stop
    when clicked on. (maybe add a MouseListener to each sortPanel either in
    MainWindow or in the class definition if possible, then a mouse click
    will invoke the listener and sleep until another click is done)

--- SortVisualizer/src/MainWindow.java
+++ SortVisualizer/src/MainWindow.java
@@ -1,27 +1,101 @@
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 import javax.swing.*;
+
 public class MainWindow {
-	JFrame frame;
-	sortPanel selectionSort;
+	private JFrame frame;
+	private sortPanel selectionSort, insertionSort, mergeSort, quickSort;
+	private JPanel buttonPanel;
+	private JEditorPane numbersPane;
+	private boolean started;
+	private JButton setValuesButton, startButton;
+	private JLabel instructionsLabel;
+	private ArrayList<Integer> numList;
+	
 	public MainWindow() {
+		started = false;	
+		numList = new ArrayList<Integer>();
 		frame = new JFrame("Sorting Algorithms Visualizer");
-		ArrayList<Integer> list = new ArrayList<Integer>();
-		list.add(5);
-		list.add(83);
-		list.add(15);
-		list.add(23);
-		list.add(100);
-		list.add(53);
-		list.add(29);
-		selectionSort = new sortPanel(list);
-		frame.setSize(500, 500);
+		frame.setLayout(new GridLayout(3,2,10,10));
+		frame.setLocation(5,5);
+		
+		selectionSort = new sortPanel("Selection Sort");
+		insertionSort = new sortPanel("Insertion Sort");
+		mergeSort = new sortPanel("Merge Sort");
+		quickSort = new sortPanel("Quick Sort");
+		
+		numbersPane = new JEditorPane();
+		
+		ButtonListener buttonListener = new ButtonListener();
+		instructionsLabel = new JLabel("<html>Enter numbers that you would like to sort, separated by spaces (positive integers only).</html>");
+		setValuesButton = new JButton("Set Values");
+		setValuesButton.setToolTipText("Use the entered values for sorting");
+		setValuesButton.addActionListener(buttonListener);
+		startButton = new JButton(" ");
+		startButton.setText("Start");
+		startButton.setToolTipText("Start sort");
+		startButton.addActionListener(buttonListener);
+		buttonPanel = new JPanel();
+		buttonPanel.setLayout(new BoxLayout(buttonPanel,BoxLayout.PAGE_AXIS));
+		buttonPanel.add(instructionsLabel);
+		buttonPanel.add(setValuesButton);
+		buttonPanel.add(startButton);
+		
+		frame.setSize(800, 800);
+		frame.setResizable(false);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.add(selectionSort);
-		
+		frame.add(insertionSort);
+		frame.add(mergeSort);
+		frame.add(quickSort);
+		frame.add(buttonPanel);
+		frame.add(numbersPane);
 		frame.setVisible(true);
 	}
-	public static void main(String[] args){
+	public void setValues(String nums){
+		String[] numArray = nums.split(" ");
+		numList.clear();
+		for(String s : numArray) {
+			if (s.matches("^[0-9]*$")){
+				numList.add(Integer.parseInt(s));
+			}
+		}
+		selectionSort.setList(numList);
+		insertionSort.setList(numList);
+		mergeSort.setList(numList);
+		quickSort.setList(numList);
+	}
+	public boolean isStarted(){
+		return started;
+	}
+	public void start(){
+		started = !started;
+		if (started){
+			startButton.setText("Stop");
+			startButton.setToolTipText("Stop sort");
+		} else {
+			startButton.setText("Start");
+			startButton.setToolTipText("Start/Resume sort");
+		}
+	}
+
+	public static void main(String[] args) throws InterruptedException {
 		MainWindow m = new MainWindow();
 	}
+	class ButtonListener implements ActionListener {
+
+		public void actionPerformed(ActionEvent event) {
+			if (event.getSource() == setValuesButton){
+				setValues(numbersPane.getText());
+				frame.repaint();
+			} else if (event.getSource() == startButton){
+				start();
+			}
+		}
+		
+	}
+
 }
--- SortVisualizer/src/sortPanel.java
+++ SortVisualizer/src/sortPanel.java
@@ -4,37 +4,61 @@ import java.awt.LayoutManager;
 import java.util.ArrayList;
 import javax.swing.JPanel;
 
-
 public class sortPanel extends JPanel {
-	ArrayList<Integer> list;
-	int barWidth;
-	int hPad;
-	int hRatio = 10; //ratio between width of bars and padding
-	int vPad = 5;
-	float vScale;
-	public sortPanel(ArrayList<Integer> list) {
+	private ArrayList<Integer> list;
+	private int hPad;
+	private int hRatio = 10; // ratio between width of bars and padding
+	private int vPad = 5;
+	private float vScale;
+	private String name;
+
+	public sortPanel(String name) {
 		super();
-		this.list = list;
+		list = new ArrayList<Integer>();
+		this.name = name;
 	}
-	public int getMaxIndex(){
+
+	public int getMaxIndex() {
 		int max = 0;
-		for (int i = 1; i < list.size();i++){
-			if (list.get(i) > list.get(max)){
+		for (int i = 1; i < list.size(); i++) {
+			if (list.get(i) > list.get(max)) {
 				max = i;
 			}
 		}
 		return max;
 	}
-	
-	
-	public void paintComponent(Graphics g){
-		Graphics2D g2d = (Graphics2D)g;
-		hPad = this.getWidth()/((hRatio+1)*list.size()+1);
-		vScale = (this.getHeight()-2*vPad)/this.list.get(this.getMaxIndex());
-		for(int i = 0; i < list.size();i++){
-			g2d.fillRect(hPad*((hRatio+1)*i + 1), vPad, hRatio*hPad, Math.round(list.get(i)*vScale));
+
+	public int getListSize() {
+		return list.size();
+	}
+
+	public void addNum(int num) {
+		list.add(num);
+	}
+	public void setList(ArrayList<Integer> list){
+		this.list = list;
+	}
+
+	public void swap(int i1, int i2) {
+		int temp = list.get(i1);
+		list.set(i1, list.get(i2));
+		list.set(i2, temp);
+	}
+
+	public void paintComponent(Graphics g) {
+		Graphics2D g2d = (Graphics2D) g;
+		g2d.clearRect(0, 0, this.getWidth(), this.getHeight());
+		g2d.drawString(name, 5, this.getHeight() - 5);
+		if (this.getListSize() > 0) {
+			hPad = this.getWidth() / ((hRatio + 1) * list.size() + 1);
+			vScale = (this.getHeight() - 2 * vPad - g2d.getFont().getSize())
+					/ (float)list.get(this.getMaxIndex());
+			g2d.drawRect(0, 0, this.getWidth(), this.getHeight());
+			for (int i = 0; i < list.size(); i++) {
+				g2d.fillRect(hPad * ((hRatio + 1) * i + 1), vPad,
+						hRatio * hPad, Math.round(list.get(i) * vScale));
+			}
 		}
-		
-		
+
 	}
 }
